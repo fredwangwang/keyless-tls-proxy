1464f19dba618a48bbf6ce40673db856f9983782
Huan Wang <huwang@omnissa.com>
2026-06-17 09:51:40 -0600

    fixies

--- internal/kspregister/register_windows.go
+++ internal/kspregister/register_windows.go
@@ -2,158 +2,130 @@
 
 package kspregister
 
+/*
+#cgo LDFLAGS: -lbcrypt -lncrypt
+#include <windows.h>
+#include <bcrypt.h>
+#include <ncrypt.h>
+
+#ifndef NT_SUCCESS
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+#endif
+
+NTSTATUS WINAPI BCryptRegisterProvider(LPCWSTR pszProvider, ULONG ulFlags, PCRYPT_PROVIDER_REG pReg);
+NTSTATUS WINAPI BCryptUnregisterProvider(LPCWSTR pszProvider);
+NTSTATUS WINAPI BCryptAddContextFunction(ULONG dwTable, LPCWSTR pszContext, ULONG dwInterface, LPCWSTR pszFunction, ULONG dwPosition);
+NTSTATUS WINAPI BCryptAddContextFunctionProvider(ULONG dwTable, LPCWSTR pszContext, ULONG dwInterface, LPCWSTR pszFunction, LPCWSTR pszProvider, ULONG dwPosition);
+NTSTATUS WINAPI BCryptRemoveContextFunctionProvider(ULONG dwTable, LPCWSTR pszContext, ULONG dwInterface, LPCWSTR pszFunction, LPCWSTR pszProvider);
+
+static PWSTR tpmcertAlgorithmNames[1] = { (PWSTR)NCRYPT_KEY_STORAGE_ALGORITHM };
+
+static CRYPT_INTERFACE_REG tpmcertAlgorithmClass = {
+    NCRYPT_KEY_STORAGE_INTERFACE,
+    CRYPT_LOCAL,
+    1,
+    tpmcertAlgorithmNames
+};
+
+static PCRYPT_INTERFACE_REG tpmcertAlgorithmClasses[1] = { &tpmcertAlgorithmClass };
+
+static CRYPT_IMAGE_REG tpmcertKspImage = {
+    (PWSTR)L"tpmcert_ksp.dll",
+    1,
+    tpmcertAlgorithmClasses
+};
+
+static CRYPT_PROVIDER_REG tpmcertKspProvider = {
+    0,
+    NULL,
+    &tpmcertKspImage,
+    NULL
+};
+
+static NTSTATUS tpmcertRegisterProvider(void) {
+    return BCryptRegisterProvider(
+        L"TPM Certificate Key Storage Provider",
+        0,
+        &tpmcertKspProvider);
+}
+
+static NTSTATUS tpmcertAddContextFunction(void) {
+    return BCryptAddContextFunction(
+        CRYPT_LOCAL,
+        NULL,
+        NCRYPT_KEY_STORAGE_INTERFACE,
+        NCRYPT_KEY_STORAGE_ALGORITHM,
+        CRYPT_PRIORITY_TOP);
+}
+
+static NTSTATUS tpmcertAddContextFunctionProvider(void) {
+    return BCryptAddContextFunctionProvider(
+        CRYPT_LOCAL,
+        NULL,
+        NCRYPT_KEY_STORAGE_INTERFACE,
+        NCRYPT_KEY_STORAGE_ALGORITHM,
+        L"TPM Certificate Key Storage Provider",
+        CRYPT_PRIORITY_TOP);
+}
+
+static NTSTATUS tpmcertRemoveContextFunctionProvider(void) {
+    return BCryptRemoveContextFunctionProvider(
+        CRYPT_LOCAL,
+        NULL,
+        NCRYPT_KEY_STORAGE_INTERFACE,
+        NCRYPT_KEY_STORAGE_ALGORITHM,
+        L"TPM Certificate Key Storage Provider");
+}
+
+static NTSTATUS tpmcertUnregisterProvider(void) {
+    return BCryptUnregisterProvider(L"TPM Certificate Key Storage Provider");
+}
+*/
+import "C"
+
 import (
 	"fmt"
 	"unsafe"
 
-	"tpm-cert-proxy/internal/kspcommon"
-
 	"golang.org/x/sys/windows"
 )
 
-const (
-	cryptLocal                = 0
-	ncryptKeyStorageInterface = 0x00010000
-	cryptPriorityTop          = 0
-)
-
-var (
-	modBCrypt = windows.NewLazySystemDLL("bcrypt.dll")
-
-	procBCryptRegisterProvider              = modBCrypt.NewProc("BCryptRegisterProvider")
-	procBCryptUnregisterProvider            = modBCrypt.NewProc("BCryptUnregisterProvider")
-	procBCryptAddContextFunction            = modBCrypt.NewProc("BCryptAddContextFunction")
-	procBCryptAddContextFunctionProvider    = modBCrypt.NewProc("BCryptAddContextFunctionProvider")
-	procBCryptRemoveContextFunctionProvider = modBCrypt.NewProc("BCryptRemoveContextFunctionProvider")
-	procBCryptEnumRegisteredProviders       = modBCrypt.NewProc("BCryptEnumRegisteredProviders")
-	procBCryptFreeBuffer                    = modBCrypt.NewProc("BCryptFreeBuffer")
-)
-
-type cryptInterfaceReg struct {
-	dwInterface    uint32
-	dwFlags        uint32
-	cFunctions     uint32
-	rgpszFunctions uintptr
-}
-
-type cryptImageReg struct {
-	pszImage      *uint16
-	cInterfaces   uint32
-	rgpInterfaces uintptr
-}
-
-type cryptProviderReg struct {
-	cInterfaces   uint32
-	rgpInterfaces uintptr
-	pImage        uintptr
-	pFunction     uintptr
-}
-
-type cryptProviders struct {
-	cProviders     uint32
-	rgpszProviders uintptr
-}
-
 func Register() error {
-	providerName, err := windows.UTF16PtrFromString(kspcommon.ProviderName)
-	if err != nil {
-		return err
+	if st := C.tpmcertRegisterProvider(); st != 0 {
+		return fmt.Errorf("BCryptRegisterProvider: NTSTATUS 0x%08X", uint32(st))
 	}
-	dllName, err := windows.UTF16PtrFromString(kspcommon.KSPLibrary)
-	if err != nil {
-		return err
+	if st := C.tpmcertAddContextFunction(); st != 0 {
+		return fmt.Errorf("BCryptAddContextFunction: NTSTATUS 0x%08X", uint32(st))
 	}
-	keyStorageAlg, err := windows.UTF16PtrFromString("KEY_STORAGE")
-	if err != nil {
-		return err
-	}
-
-	algNames := [1]*uint16{keyStorageAlg}
-	algClass := cryptInterfaceReg{
-		dwInterface:    ncryptKeyStorageInterface,
-		dwFlags:        cryptLocal,
-		cFunctions:     1,
-		rgpszFunctions: uintptr(unsafe.Pointer(&algNames[0])),
-	}
-	algClasses := [1]cryptInterfaceReg{algClass}
-	image := cryptImageReg{
-		pszImage:      dllName,
-		cInterfaces:   1,
-		rgpInterfaces: uintptr(unsafe.Pointer(&algClasses[0])),
-	}
-	provider := cryptProviderReg{
-		cInterfaces:   0,
-		rgpInterfaces: 0,
-		pImage:        uintptr(unsafe.Pointer(&image)),
-		pFunction:     0,
-	}
-
-	status, _, _ := procBCryptRegisterProvider.Call(
-		uintptr(unsafe.Pointer(providerName)),
-		0,
-		uintptr(unsafe.Pointer(&provider)),
-	)
-	if status != 0 {
-		return fmt.Errorf("BCryptRegisterProvider: NTSTATUS 0x%08X", status)
-	}
-
-	status, _, _ = procBCryptAddContextFunction.Call(
-		cryptLocal,
-		0,
-		ncryptKeyStorageInterface,
-		uintptr(unsafe.Pointer(keyStorageAlg)),
-		cryptPriorityTop,
-	)
-	if status != 0 {
-		return fmt.Errorf("BCryptAddContextFunction: NTSTATUS 0x%08X", status)
-	}
-
-	status, _, _ = procBCryptAddContextFunctionProvider.Call(
-		cryptLocal,
-		0,
-		ncryptKeyStorageInterface,
-		uintptr(unsafe.Pointer(keyStorageAlg)),
-		uintptr(unsafe.Pointer(providerName)),
-		cryptPriorityTop,
-	)
-	if status != 0 {
-		return fmt.Errorf("BCryptAddContextFunctionProvider: NTSTATUS 0x%08X", status)
+	if st := C.tpmcertAddContextFunctionProvider(); st != 0 {
+		return fmt.Errorf("BCryptAddContextFunctionProvider: NTSTATUS 0x%08X", uint32(st))
 	}
 	return nil
 }
 
 func Unregister() error {
-	providerName, err := windows.UTF16PtrFromString(kspcommon.ProviderName)
-	if err != nil {
-		return err
+	if st := C.tpmcertRemoveContextFunctionProvider(); st != 0 {
+		return fmt.Errorf("BCryptRemoveContextFunctionProvider: NTSTATUS 0x%08X", uint32(st))
 	}
-	keyStorageAlg, err := windows.UTF16PtrFromString("KEY_STORAGE")
-	if err != nil {
-		return err
-	}
-
-	status, _, _ := procBCryptRemoveContextFunctionProvider.Call(
-		cryptLocal,
-		0,
-		ncryptKeyStorageInterface,
-		uintptr(unsafe.Pointer(keyStorageAlg)),
-		uintptr(unsafe.Pointer(providerName)),
-	)
-	if status != 0 {
-		return fmt.Errorf("BCryptRemoveContextFunctionProvider: NTSTATUS 0x%08X", status)
-	}
-
-	status, _, _ = procBCryptUnregisterProvider.Call(uintptr(unsafe.Pointer(providerName)))
-	if status != 0 {
-		return fmt.Errorf("BCryptUnregisterProvider: NTSTATUS 0x%08X", status)
+	if st := C.tpmcertUnregisterProvider(); st != 0 {
+		return fmt.Errorf("BCryptUnregisterProvider: NTSTATUS 0x%08X", uint32(st))
 	}
 	return nil
 }
 
 func EnumProviders() ([]string, error) {
+	mod := windows.NewLazySystemDLL("bcrypt.dll")
+	procEnum := mod.NewProc("BCryptEnumRegisteredProviders")
+	procFree := mod.NewProc("BCryptFreeBuffer")
+
+	type cryptProviders struct {
+		cProviders     uint32
+		rgpszProviders *uintptr
+	}
+
 	var cb uint32
 	var buf *cryptProviders
-	status, _, _ := procBCryptEnumRegisteredProviders.Call(
+	status, _, _ := procEnum.Call(
 		uintptr(unsafe.Pointer(&cb)),
 		uintptr(unsafe.Pointer(&buf)),
 	)
@@ -163,12 +135,12 @@ func EnumProviders() ([]string, error) {
 	if buf == nil {
 		return nil, nil
 	}
-	defer procBCryptFreeBuffer.Call(uintptr(unsafe.Pointer(buf)))
+	defer procFree.Call(uintptr(unsafe.Pointer(buf)))
 
 	count := int(buf.cProviders)
 	out := make([]string, 0, count)
 	for i := 0; i < count; i++ {
-		ptr := *(*uintptr)(unsafe.Pointer(buf.rgpszProviders + uintptr(i)*unsafe.Sizeof(uintptr(0))))
+		ptr := *(*uintptr)(unsafe.Add(unsafe.Pointer(buf.rgpszProviders), i*int(unsafe.Sizeof(uintptr(0)))))
 		out = append(out, windows.UTF16PtrToString((*uint16)(unsafe.Pointer(ptr))))
 	}
 	return out, nil
