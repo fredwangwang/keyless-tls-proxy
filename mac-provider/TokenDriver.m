4d32a36d08540e0184e75f68132d7f4abd6972f4
Huan Wang <fredwanghuan@gmail.com>
2026-08-10 22:39:20 -0400

    Load extension certs from shared App Group config and remove static cert bundling

--- mac-provider/TokenDriver.m
+++ mac-provider/TokenDriver.m
@@ -3,19 +3,39 @@
 #import <os/log.h>
 #include "libctkbridge.h"
 
-static void initBridgeIfNeeded(void) {
+static NSString *getAppGroupID(void) {
+    static NSString *cachedAppGroupID = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         NSBundle *bundle = [NSBundle bundleForClass:[MacTokenDriver class]];
-        NSString *ca = [bundle pathForResource:@"ca" ofType:@"crt" inDirectory:@"certs"];
-        NSString *cert = [bundle pathForResource:@"client" ofType:@"crt" inDirectory:@"certs"];
-        NSString *key = [bundle pathForResource:@"client" ofType:@"key" inDirectory:@"certs"];
-        
-        if (!ca || !cert || !key) {
-            os_log_error(OS_LOG_DEFAULT, "MacTokenDriver error: missing certificate file(s) in bundle certs/ (ca: %@, cert: %@, key: %@)", ca, cert, key);
+        cachedAppGroupID = [bundle objectForInfoDictionaryKey:@"AppGroupID"];
+        if (!cachedAppGroupID || cachedAppGroupID.length == 0) {
+            cachedAppGroupID = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroupID"];
+        }
+    });
+    return cachedAppGroupID;
+}
+
+static void initBridgeIfNeeded(void) {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSString *appGroupID = getAppGroupID();
+        NSUserDefaults *defaults = nil;
+        if (appGroupID.length > 0) {
+            defaults = [[NSUserDefaults alloc] initWithSuiteName:appGroupID];
+        }
+
+        NSString *addr = [defaults stringForKey:@"serverAddr"] ?: [defaults stringForKey:@"ServerAddress"];
+        NSString *ca   = [defaults stringForKey:@"caContent"]   ?: [defaults stringForKey:@"caPath"];
+        NSString *cert = [defaults stringForKey:@"certContent"] ?: [defaults stringForKey:@"certPath"];
+        NSString *key  = [defaults stringForKey:@"keyContent"]  ?: [defaults stringForKey:@"keyPath"];
+
+        if (addr.length == 0 || ca.length == 0 || cert.length == 0 || key.length == 0) {
+            os_log_error(OS_LOG_DEFAULT, "MacTokenDriver error: missing shared configuration in App Group (%{public}@). addr: %{public}@, ca len: %lu, cert len: %lu, key len: %lu",
+                         appGroupID ?: @"nil", addr ?: @"nil", (unsigned long)ca.length, (unsigned long)cert.length, (unsigned long)key.length);
         } else {
-            os_log(OS_LOG_DEFAULT, "MacTokenDriver initializing ctk bridge with CA: %@, cert: %@, key: %@", ca, cert, key);
-            int res = ctk_bridge_init_opts("192.168.0.133:50051", (char *)ca.UTF8String, (char *)cert.UTF8String, (char *)key.UTF8String);
+            os_log(OS_LOG_DEFAULT, "MacTokenDriver initializing ctk bridge with shared config from App Group (%{public}@), server: %{public}@", appGroupID, addr);
+            int res = ctk_bridge_init_opts((char *)addr.UTF8String, (char *)ca.UTF8String, (char *)cert.UTF8String, (char *)key.UTF8String);
             os_log(OS_LOG_DEFAULT, "MacTokenDriver ctk_bridge_init_opts res: %d", res);
         }
     });
